be659debb5fa850615d1f0dd66839ba1697d0958
fredrik879 <fredrik.a@protonmail.ch>
2025-04-26 19:30:04 +0200

    Update server.js

--- document-qa-system/backend/server.js
+++ document-qa-system/backend/server.js
@@ -1,21 +1,25 @@
-import Fastify from 'fastify';
 import dotenv from 'dotenv';
-import { MongoClient } from 'mongodb';
 
-// Load environment variables
-dotenv.config();
+import Fastify from 'fastify';
+import fastifyMultipart from '@fastify/multipart';
+import mongoose from 'mongoose';
+import path from 'path';
+import { uploadFileToR2 } from './utils/r2.js';
 
+// Configure Fastify
 const fastify = Fastify({
   logger: true,
 });
 
-// Initialize MongoDB client
-const mongoClient = new MongoClient(process.env.MONGODB_URI);
+fastify.register(fastifyMultipart);
+
+// Load environment variables
+dotenv.config();
 
 // Connect to MongoDB
 async function connectToMongoDB() {
   try {
-    await mongoClient.connect();
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log('Connected to MongoDB');
   } catch (error) {
     console.error('Failed to connect to MongoDB:', error);
@@ -50,7 +54,36 @@ fastify.get('/', function (request, reply) {
 // Upload PDF
 
 // Add data
-fastify.post('/add-data', function (request, reply) {
+fastify.post('/add-data', async (request, reply) => {
+  // Upload file to R2
+  try {
+    const data = await request.file();
+
+    if (!data) {
+      return reply.code(400).send({ error: 'No file to upload' });
+    }
+
+    // Check file extension
+    const fileExtension = path.extname(data.filename).toLowerCase();
+
+    if (fileExtension !== '.pdf') {
+      return reply.code(400).send({ error: 'Only PDF files are supported' });
+    }
+
+    // Get file buffer
+    const fileBuffer = await data.toBuffer();
+
+    // Upload file to R2
+    const r2Result = await uploadFileToR2(fileBuffer, data.filename);
+    const r2Url = `${process.env.CLOUDFLARE_R2_SUBDOMAIN}/${r2Result.filename}`;
+
+    return {
+      r2Url: r2Url,
+    };
+  } catch (err) {
+    console.log(err);
+  }
+
   // Extract text from PDF
   // Convert text to vector embeddings
 });
